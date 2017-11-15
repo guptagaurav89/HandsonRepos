741b16777b19dfec52909e9e4a43f7d1018f64b7
Bunny <Bunny@HP>
2017-11-15 21:20:45 +0530

    Entity Framework and LINQ examples

---
+++ EntityFrameworkConcepts/EntityFrameworkConcepts/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UnderwritingEntitiesContainer db = new UnderwritingEntitiesContainer())
+            {
+                db.Database.Log = Console.WriteLine;
+
+                //db.Configuration.AutoDetectChangesEnabled = false;
+                //db.Configuration.ProxyCreationEnabled = false;
+
+                db.headers.Add(new header() { Id = 2,PolicyReference = "P02" });
+
+                db.headers.Find(1).PolicyReference += "-1";
+
+                Console.WriteLine(db.ChangeTracker.Entries().Count());
+                Console.WriteLine(db.ChangeTracker.Entries().Where(e => e.State != System.Data.Entity.EntityState.Unchanged).Count());
+                db.ChangeTracker.DetectChanges();
+                Console.WriteLine(db.ChangeTracker.Entries().Where(e => e.State != System.Data.Entity.EntityState.Unchanged).Count());
+
+                var changedheader = db.ChangeTracker.Entries().Where(e => e.State == System.Data.Entity.EntityState.Modified).First();
+
+                Console.WriteLine("ID - " + changedheader.OriginalValues["Id"] + " Modified? " +changedheader.Property("Id").IsModified);
+                Console.WriteLine(changedheader.OriginalValues["PolicyReference"] + " is now " + changedheader.CurrentValues["PolicyReference"]);
+
+                Console.WriteLine(db.ChangeTracker.Entries().Last().Entity.GetType().FullName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinqExamples/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace LinqExamples
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                PADBEntities Db = new PADBEntities();
+
+                Db.Database.Log = Console.WriteLine;
+                ///Simple LINQ Query
+                //var emp = (from e in Db.HRISMSTEmployees
+                //           //where e.txtcompany == "EVERVE"
+                //           orderby e.txtempname
+                //           select e).ToList();
+                //foreach (var item in emp)
+                //{
+                //    Console.WriteLine("Company Name " + item.Key + " EmpName :" + item.Count);
+                //}
+
+                ///Using Join 
+                #region "Using Join"
+                //single column join
+                var res = from e in Db.Albums
+                          join a in Db.Artists on e.ArtistId equals a.ArtistId
+                          select new
+                          {
+                              AlbumName = e.Title,
+                              AlbumArtist = a.Name
+                          };//).OrderBy(x => x.AlbumArtist).ThenBy(y => y.AlbumName).ToList();
+                
+                //foreach (var item in res)
+                    //Console.WriteLine(" Artist : " + item.AlbumArtist + " \tAlbum : " + item.AlbumName);
+
+                //Group join
+                var hell = from e in Db.Artists
+                           join a in Db.Albums
+                           on  e.ArtistId equals a.ArtistId
+                           into q
+                           select new { Artist = e.Name, AlbumList = q };
+
+                //foreach (var ch in hell)
+                //{
+                //    Console.WriteLine("Artist : " + ch.Artist);
+                //    foreach (var dd in ch.AlbumList)
+                //        Console.Write(" \n \t Albums : " + dd.Title);
+                //}
+                foreach (var ch in hell)
+                {
+                    try
+                    {
+                        Console.WriteLine("Artist : " + ch.Artist);
+                        Console.Write(" \t Albums : " + ch.AlbumList.FirstOrDefault().Title);
+                    }
+                    catch (Exception)
+                    { }
+                }
+
+
+                #endregion
+
+                ///Using Group By Clause
+                #region "Group By Clause"
+                //var emp = (from e in Db.HRISMSTEmployees
+                //           group e by e.txtcompany into g
+                //           select g).ToList();
+                //foreach (var item in emp)
+                //{
+                //    foreach (var empl in item)
+                //    {
+                //        Console.WriteLine("Company Name " + item.Key + " EmpName :" + empl.txtempname);
+                //    }
+                //    //Console.WriteLine("Company Name " + item.Key + " EmpName :" + item.FirstOrDefault().txtempname);
+
+                //    //Using count with group by clause
+                //    Console.WriteLine("Company Name " + item.Key + " EmpName :" + item.Count());
+                //} 
+                #endregion
+
+
+                ///Using Dynamic filters
+                //Dynfilter("GenreId", "10");
+                //Console.WriteLine();
+                //Dynfilter("ArtistId", "248");
+                
+                
+                ///Using SubQuery
+                
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                
+                //throw;
+            }
+            
+        }
+
+        static void Dynfilter(string Col, string val)
+        {
+            PADBEntities Db = new PADBEntities();
+            var valbum = (from a in Db.Albums select a).ToArray();
+
+            PropertyInfo prp = valbum[0].GetType().GetProperty(Col);
+
+            var result = valbum.Where(x => prp.GetValue(x, null).ToString() == val);
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(" Name " + r.Title + " GenreId :" + r.GenreId + " ArtistId : " + r.ArtistId);
+            }
+
+        }
+    }
+}
