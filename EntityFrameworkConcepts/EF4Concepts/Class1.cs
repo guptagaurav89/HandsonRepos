941b8e5df2b4385c859b35ace0d2398859852899
Gaurav <Gaurav@Office>
2017-11-21 16:00:59 +0530

    EF with ObjectContext

---
+++ EntityFrameworkConcepts/EF4Concepts/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF4Concepts
+{
+    public class Class1
+    {
+        TestEFEntities db;
+
+        public void GetDepartmentDetails()
+        {
+            try
+            {
+
+                db = new TestEFEntities();
+                var dept = from d in db.Departments
+                           select d;
+
+                foreach (var dd in dept)
+                    Console.WriteLine("Department Name : " + dd.DeptName + " Department Head Id " + dd.EmployeeReference.EntityKey.EntityKeyValues.First().Value);
+                //Console.WriteLine("Department Name : " + dd.DeptName + " Department Head Id ");// + dd.Employee.Empid);
+            }
+            catch (Exception)
+            {
+                Console.Write("Err");
+            }
+        }
+
+        public void GetDepartmentDetails(int deptid)
+        {
+            try
+            {
+                db = new TestEFEntities();
+                var dept = (from d in db.Departments
+                            where d.DeptId == deptid
+                            select d).FirstOrDefault();
+
+                Console.WriteLine("Department Name : " + dept.DeptName + " Department Head Id " + dept.EmployeeReference.EntityKey.EntityKeyValues.First().Value);
+                //Console.WriteLine("Department Name : " + dept.DeptName + " Department Head Id ");// + dept.Employee.Empid);
+            }
+            catch (Exception)
+            {
+                Console.Write("Err");
+            }
+        }
+    }
+}
---
+++ EntityFrameworkConcepts/EF4Concepts/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF4Concepts
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 c = new Class1();
+            c.GetDepartmentDetails();
+
+            c.GetDepartmentDetails(2);       }
+
+    }
+}
