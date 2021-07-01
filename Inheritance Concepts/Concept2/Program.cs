e757e037f25431aa0ae1b8e051dee90c7d11c9b0
@guptagaurav89 <ggtech.89@gmail.com>
2021-07-01 10:13:42 +0530

    Added more examples

---
+++ Inheritance Concepts/Concept2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Concept2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basechild fc = new Firstchild();
+            fc.Display();
+            Basechild sc = new Secondchild();
+            sc.Display();
+            Firstchild sfc = new Secondchild();
+            sfc.Display();
+        }       
+    }
+
+    public class Basechild
+    {
+        public virtual void Display(){Console.WriteLine("Basechild");}
+    }
+    public class Firstchild : Basechild
+    {
+        public new virtual void Display()
+        {
+            Console.WriteLine("FirstChild");
+        }
+        // public override void Display()
+        // {
+        //     Console.WriteLine("FirstChild");
+        // }
+    }
+
+    public class Secondchild : Firstchild
+    {
+        public override void Display()
+        {
+            Console.WriteLine("SecondChild");
+        }
+    }
+}
